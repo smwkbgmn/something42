eacadac4323ee4c9a7249a24ba8b6a9b7704f0b2
Albert Lajara <110899377+LAG-jara@users.noreply.github.com>
2024-06-24 20:41:24 +0200

    Update fetch.js
    modo: cors added

--- fetch.js
+++ fetch.js
@@ -7,7 +7,8 @@ function fetchIds() {
 	const requestOptions = {
 	  method: "GET",
 	  headers: myHeaders,
-	  redirect: "follow"
+	  redirect: "follow",
+	  modo: "cors"
 	};
 
 	fetch("https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-ea1f8d209fe0b6bde730011e41f792f77737f9eb5f80a428e93ed76351f8e2b4&redirect_uri=https%3A%2F%2Fsmwkbgmn.github.io%2Fsomthing42%2F&response_type=code", requestOptions)
