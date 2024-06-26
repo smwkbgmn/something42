2be718b1a9275047fe025c01b6478f4bb17a8be1
smwkbgmn <6moguai9@gmail.com>
2024-06-26 17:47:50 +0900

    test

--- fetch_cors_proxy.js
+++ fetch_cors_proxy.js
@@ -3,7 +3,7 @@ const redirectUri = 'https://smwkbgmn.github.io/somthing42/';
 // const corsProxy = 'https://cors-anywhere.herokuapp.com/';
 // const corsProxy = 'https://something42-d4bd81072306.herokuapp.com/proxy?url=';
 const corsProxy = 'https://something42-d4bd81072306.herokuapp.com/';
-let accessToken = "1b82402c89be2564c586d8c7523928e2ea74e28a2e57628c966bf9095e5132c5";
+let accessToken = "00808be17e9c50a39d4e585ee3e5bf01afe5617b24f9fffc3fb2f877d272c156";
 
 document.getElementById('loginButton').addEventListener('click', login);
 document.getElementById('fetchButton').addEventListener('click', fetchData);
@@ -33,7 +33,7 @@ function fetchData() {
 
     fetch(fullUrl, {
         headers: {
-            'Authorization': `Bearer 1b82402c89be2564c586d8c7523928e2ea74e28a2e57628c966bf9095e5132c5`,
+            'Authorization': `Bearer 00808be17e9c50a39d4e585ee3e5bf01afe5617b24f9fffc3fb2f877d272c156`,
 			'Origin': 'https://smwkbgmn.github.io'
         }
     })
